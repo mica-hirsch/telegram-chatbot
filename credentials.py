bcbac56077aca76421dd5848f50913e4dbe262fa
Micaela Hirsch Romerstein <hirschmicaela@gmail.com>
2020-10-06 19:44:59 +0200

    Update credentials.py

--- credentials.py
+++ credentials.py
@@ -1,3 +1,3 @@
-bot_token = "1393605195:AAFcOfJvq5v86UXZ9_67h2hGRFlm-y_Y5Xk"
-bot_user_name = "@ssi_prueba_bot"
-URL = "https://ssi-bot-heroku.herokuapp.com/"
+bot_token = "[your bot token here]"
+bot_user_name = "[your bot's name here]"
+URL = "[your heroku app here"
